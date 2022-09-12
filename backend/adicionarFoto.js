2077b3daa57c65cf794af6387224103768db933f
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2022-09-12 08:50:12 -0300

    Add files via upload

---
+++ backend/adicionarFoto.js
@@ -0,0 +1,21 @@
+
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('flImage');
+
+photo.addEventListener('click', () => {
+    file.click();
+});
+
+file.addEventListener('change', () => {
+    if (file.files.lenght <= 0) {
+        return;
+    }
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            photo.src = reader.result;
+        }
+        reader.readAsDataURL(file.files[0]);
+    })
+    
