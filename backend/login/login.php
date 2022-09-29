18bd93481c8a3d826cdf64a61013f452ab416f01
NavyBlue1 <navybluue.1@gmail.com>
2022-09-29 09:40:45 -0300

    login.php funcionando, usando outra tabela

--- backend/login/login.php
+++ backend/login/login.php
@@ -1,20 +1,17 @@
 <?php
 include_once('../../database/config.php');
 
-$email=$_POST["email"];
-$password=$_POST["senha"];
-$idUsuario=00000000000;
+$email=$_POST['email'];
+$senha=$_POST['senha'];
 
-
-$stmt = $conn->prepare("INSERT INTO tb_login VALUES (:idUsuario, :email	,:senha)");
-$stmt->bindParam(':idUsuario',$idUsuario);
+$stmt = $conn->prepare("INSERT INTO tb_login_teste (email, senha) VALUES (:email, :senha)");
 $stmt->bindParam(':email', $email);
 $stmt->bindParam(':senha',$senha);
 $stmt->execute();
 
-
-
-
+if($stmt == true){
+    echo "Dados inseridos!";
+}
 
 
 ?>
