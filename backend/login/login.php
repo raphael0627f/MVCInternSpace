0cebafacf5168d7264a365efd37a0b92a8ba0185
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2022-09-30 11:09:18 -0300

    Update login.php

--- backend/login/login.php
+++ backend/login/login.php
@@ -3,12 +3,15 @@ include_once('../../database/config.php');
 
 $email=$_POST['email'];
 $senha=$_POST['senha'];
+$user = 2;
 
-$stmt = $conn->prepare("INSERT INTO tb_login_teste (email, senha) VALUES (:email, :senha)");
+$stmt = $conn->prepare("INSERT INTO tb_login (idUsuario ,email, senha) VALUES (:idUsuario, :email, :senha)");
+$stmt->bindParam(':idUsuario',$user);
 $stmt->bindParam(':email', $email);
 $stmt->bindParam(':senha',$senha);
 $stmt->execute();
 
+var_dump($user);
 if($stmt == true){
     echo "Dados inseridos!";
 }
