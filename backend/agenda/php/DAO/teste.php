446daa69994c46f7ea2cab45f9c034c6c33d1e99
Daniel Bernardes Cardoso <cqrdosodaniel@gmail.com>
2022-10-17 10:32:41 -0300

    Delete backend/agenda directory

--- backend/agenda/php/DAO/teste.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include('../Model/ModelAgendamento.php');
-include('./AgendamentoDAO.php');
-$teste = new AgendamentoDAO();
-$teste = $teste->adicionarAgendamento(123,'teste','teste','teste','teste');
-?> 
