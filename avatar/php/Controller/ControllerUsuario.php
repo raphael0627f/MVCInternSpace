bf785ed71c462ac67f427c83756b4372802648a3
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2022-10-19 07:26:16 -0300

    main

--- avatar/php/Controller/ControllerUsuario.php
+++ avatar/php/Controller/ControllerUsuario.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../DAO/UsuarioDAO.php');
+include_once('../DAO/UsuarioDAO.php');
 class ControllerUsuario{
     
     function adicionarUsuarioController(ModelUsuario $usuario){
