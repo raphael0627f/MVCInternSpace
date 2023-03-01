2b207a8bfdc46c7fd04a50ccde87b1e62042726a
NavyBlue1 <navybluue.1@gmail.com>
2023-03-01 18:37:56 -0300

    d

--- php/connection/conn.php
+++ php/connection/conn.php
@@ -6,7 +6,6 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'davi gay';
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
