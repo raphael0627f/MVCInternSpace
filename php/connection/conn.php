2448a7dc1725e1cf876a2c7e45cffbe8d7ea36c0
raphael0627f <raphaelferreirafo@gmail.com>
2023-03-01 19:18:42 -0300

    .

--- php/connection/conn.php
+++ php/connection/conn.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "greenlight.dev.br";
-    $username = "hjjyaccv_hjjyaccv";
+    $servername = "sql213.epizy.com";
+    $username = "epiz_33601998";
     $password = "w2Z:s7;9BYDyi5";
-    $dbname = "hjjyaccv_internspace";
+    $dbname = "epiz_33601998_internspace";
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
