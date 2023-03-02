89890da40678af36ca34f262fffac35f38c8f2ca
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2023-03-02 10:22:32 -0300

    teste mvc

--- configuration/conn.php
+++ configuration/conn.php
@@ -4,13 +4,23 @@ $username = "etpc";
 $password = "etpc1234@";
 $dbname = "capacitar";
 
+ class conn{
+   protected $conn;
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "está funcionando";
-
-} catch(PDOException $e) {
+   function __construct()
+   {
+    $this ->connectDatabase();
+   }
+   function connectDatabase(){
+    global $servername, $username, $password, $dbname;
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "está funcionando";
+ 
+  } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
+  }
+ }
 }
 ?>
