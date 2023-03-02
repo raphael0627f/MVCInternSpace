beffa9c68294a72d5be0fce167dc78e9eac6c0f4
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2023-03-02 10:02:53 -0300

    teste mvc

---
+++ controllers/clientContorller.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('./models/Client.php');
+
+
+   class clientsController{
+    private $modal;
+
+    function __construct()
+        {
+            $this->modal = '';
+        }
+
+}
+
+?>
