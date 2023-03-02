bfcda22103e855d27beb2976db134ff907cb42d8
kantsrneyrff <111061612+kantsrneyrff@users.noreply.github.com>
2023-03-02 09:38:16 -0300

    teste

--- php/connection/conn.php
+++ php/connection/conn.php
@@ -1,13 +1,16 @@
 <?php
-    $servername = "sql213.epizy.com";
-    $username = "epiz_33601998";
-    $password = "w2Z:s7;9BYDyi5";
-    $dbname = "epiz_33601998_internspace";
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'ok';
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
+$servername = "app.riaambrasil.org.br";
+$username = "etpc";
+$password = "etpc1234@";
+$dbname = "capacitar";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "está funcionando";
+
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 ?>
