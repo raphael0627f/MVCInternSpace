0b2fc7815049f35283e148af30c99c00bff9cf2e
raphael0627f <raphaelferreirafo@gmail.com>
2023-03-07 18:52:34 -0300

    .

--- private/views/includes/nav.view.php
+++ private/views/includes/nav.view.php
@@ -3,6 +3,5 @@
         <!-- Lado esquerdo -->
         <div class="lado-esquerdo">
             <a href="../index.html" class="logo"><img src="../public/assets/img/Logo_Light.png" alt="Logo do site InternSpace" class="logo"></a>
-            <a id="nome" href="resumoProfissional.html">Giovanna</a>
         </div>
     </nav>
